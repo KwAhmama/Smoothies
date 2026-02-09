10bfd832e7b19788180d6c98600a23f3c028eaf9
KwAhmama <98860158+KwAhmama@users.noreply.github.com>
2026-02-09 19:23:26 +0100

    Enhance smoothie customization and data retrieval
    Updated the smoothie ordering app to allow customization of ingredients and improved data handling.

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,41 +1,53 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
-from snowflake.snowpark.functions import col, when_matched
+from snowflake.snowpark.functions import col
+import requests
 
-# Título
-st.title(":cup_with_straw: Pending Smoothie Orders :cup_with_straw:")
+# Write directly to the app
+st.title(":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
+st.write(
+    """Choose the fruits you want in your custom Smoothie!
+    """
+)
+name_on_order = st.text_input('Name on Smothie: ')
+st.write('The name on your Smoothie will be: ', name_on_order)
 
-# CONEXIÓN: Esta línea solo funciona si estás DENTRO de Snowflake
-try:
-    session = get_active_session()
-except:
-    st.error("Error crítico: Esta app debe ejecutarse desde dentro de Snowflake (Snowsight), no desde Streamlit Cloud externo.")
-    st.stop()
+cnx = st.connection("snowflake")
+session = cnx.session()
 
-# Carga de datos
-my_dataframe = session.table("smoothies.public.orders").filter(col("ORDER_FILLED")==0)
+my_dataframe = session.table(
+    "smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
+pd_df = my_dataframe.to_pandas()
 
-# Mostrar datos si existen
-if my_dataframe.count() > 0:
-    # Convertimos a Pandas para el editor
-    editable_df = st.data_editor(my_dataframe.to_pandas())
+
+ingredients_list = st.multiselect(
+    "Choose up to 5 ingredients",
+    my_dataframe,
+    max_selections=5
+)
+
+if ingredients_list:
     
-    submitted = st.button('Submit')
-    
-    if submitted:
-        try:
-            og_dataset = session.table("smoothies.public.orders")
-            edited_dataset = session.create_dataframe(editable_df)
+    ingredients_string = ''
 
-            og_dataset.merge(
-                edited_dataset,
-                (og_dataset['ORDER_UID'] == edited_dataset['ORDER_UID']),
-                [when_matched().update({'ORDER_FILLED': edited_dataset['ORDER_FILLED']})]
-            )
-            st.success('¡Orden actualizada!', icon='👍')
-            st.experimental_rerun()
-        except Exception as e:
-            st.error(f'Algo salió mal: {e}')
-else:
-    st.success('No hay órdenes pendientes ahora mismo', icon='👍')
+    for fruit_chosen in ingredients_list:
+        ingredients_string += fruit_chosen + ' '
+
+        search_on=pd_df.loc[pd_df['FRUIT_NAME'] == fruit_chosen, 'SEARCH_ON'].iloc[0]
+        st.write('The search value for ', fruit_chosen,' is ', search_on, '.')
+
+        fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+search_on)
+        fv_dt = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+
+    st.write(ingredients_string)
+
+    my_insert_stmt = """ insert into smoothies.public.orders
+    (ingredients, name_on_order)
+    values ('""" + ingredients_string + """', '""" + name_on_order +"""')"""
+
+    time_to_insert = st.button('Submit Order')
+
+    if time_to_insert:
+        
+        session.sql(my_insert_stmt).collect()
+        st.success('Your Smoothie is ordered, '+name_on_order+'!', icon="✅")
